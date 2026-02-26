e4379f8e6232fed4edfbbdbcf04ae53ed2211bfc
Stefan Z. <s.zell.1@diezells.de>
2026-02-26 22:46:21 +0100

    initial import

---
+++ inc/reset.h
@@ -0,0 +1,93 @@
+/*-----------------------------------------------------------------------------+
+|                                                                              |
+| filename: reset.h                                                            |
+| project:  ZX Spectrum Next - RESET                                           |
+| author:   Stefan Zell                                                        |
+| date:     01/21/2026                                                         |
+|                                                                              |
++------------------------------------------------------------------------------+
+|                                                                              |
+| description:                                                                 |
+|                                                                              |
+| Application to reset the ZX Spectrum Next                                    |
+|                                                                              |
++------------------------------------------------------------------------------+
+|                                                                              |
+| Copyright (c) 01/21/2026 STZ Engineering                                     |
+|                                                                              |
+| This software is provided  "as is",  without warranty of any kind, express   |
+| or implied. In no event shall STZ or its contributors be held liable for any |
+| direct, indirect, incidental, special or consequential damages arising out   |
+| of the use of or inability to use this software.                             |
+|                                                                              |
+| Permission is granted to anyone  to use this  software for any purpose,      |
+| including commercial applications,  and to alter it and redistribute it      |
+| freely, subject to the following restrictions:                               |
+|                                                                              |
+| 1. Redistributions of source code must retain the above copyright            |
+|    notice, definition, disclaimer, and this list of conditions.              |
+|                                                                              |
+| 2. Redistributions in binary form must reproduce the above copyright         |
+|    notice, definition, disclaimer, and this list of conditions in            |
+|    documentation and/or other materials provided with the distribution.      |
+|                                                                          ;-) |
++-----------------------------------------------------------------------------*/
+
+#if !defined(__RESET_H__)
+  #define __RESET_H__
+
+/*============================================================================*/
+/*                               Includes                                     */
+/*============================================================================*/
+
+/*============================================================================*/
+/*                               Defines                                      */
+/*============================================================================*/
+
+/*============================================================================*/
+/*                               Namespaces                                   */
+/*============================================================================*/
+
+/*============================================================================*/
+/*                               Konstanten                                   */
+/*============================================================================*/
+
+/*============================================================================*/
+/*                               Variablen                                    */
+/*============================================================================*/
+
+/*============================================================================*/
+/*                               Strukturen                                   */
+/*============================================================================*/
+
+/*============================================================================*/
+/*                               Typ-Definitionen                             */
+/*============================================================================*/
+/*!
+Enumeration/list of all actions the application can execute
+*/
+typedef enum _action
+{
+  ACTION_NONE = 0,
+  ACTION_HELP,
+  ACTION_INFO,
+  ACTION_RESET
+} action_t;
+
+/*============================================================================*/
+/*                               Prototypen                                   */
+/*============================================================================*/
+
+/*============================================================================*/
+/*                               Klassen                                      */
+/*============================================================================*/
+
+/*============================================================================*/
+/*                               Implementierung                              */
+/*============================================================================*/
+
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+#endif /* __RESET_H__ */
